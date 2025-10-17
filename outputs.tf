7a5812e5a40ff063543cbded5a8eafd46be93ee6
shanky21111987 <sach.gan@gmail.com>
2025-10-17 11:49:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-value = google_compute_network.vpc,vpc_id
+value = google_compute_network.vpc.vpc_id
 }
 output "subnet_self_link" {
 value = google_compute_subnetwork.subnet.subnet_self_link
